3c64947c2a342f7d9c16403a8a676bf2910be6a2
Ryan W <ryanwhalley@gmail.com>
2018-08-21 14:21:13 +0000

    Debugging & Add association between user and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,7 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
+    @article.user = User.first # temporarily hard code to first user in DB
     # @article.save
     # redirect_to article_path(@article)
     
