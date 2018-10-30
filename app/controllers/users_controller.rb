929fe3073b6825854c6d3b9a01e617dabfd10ff3
Ryan W <ryanwhalley@gmail.com>
2018-10-30 15:59:15 +0000

    Added admin functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
   
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -26,7 +27,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    
     if @user.update(user_params)
       flash[:success] = "Account Updated Successfully"
       redirect_to articles_path
@@ -36,9 +36,16 @@ class UsersController < ApplicationController
   end
   
   def show
-    
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all related articles deleted"
+    redirect_to users_path
+  end
+  
   
   private 
     def user_params
@@ -50,8 +57,15 @@ class UsersController < ApplicationController
     end
     
     def require_same_user
-      if current_user != @user
+      if current_user != @user and !current_user.admin?
         flash[:danger] = "Permission denied"
+        redirect_to root_path
+      end
+    end
+    
+    def require_admin
+      if logged_in? and !current_user.admin?
+        flash[:danger] = "Only admin users can do this"
         redirect_to root_path
       end
     end
